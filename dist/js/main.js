613b5dac9758ca77d95320e31dbec6c00af6c2c4
zoepage <o.gasidlo@gmail.com>
2014-10-22 14:54:46 +0200

    added slideeffect for nav, not finished

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,26 @@
 $(function() {
 //    $('#logo-animation').delay(5000).addClass('animate');
 
+var header = $('header');
+event.preventDefault();
+
+    $(document).scroll(function () {
+        var scroll = $(this).scrollTop();
+        var topDist = $(".content").position();
+
+        if (scroll > (topDist.top + 60)) {
+            header.css({"position":"relative","top": scroll}).animate({'height':'20'}, 800).addClass('close');
+        } else {
+            header.css({"position":"static","top":"auto"}).removeClass('close');
+        }
+
+
+    });
+
+    $('header').on('click', function(eve){
+        if(header.hasClass('close')) {
+            alert('Hello! Mach mich auf! Gruß, Navigation <3');
+        }
+    });
+
 });
