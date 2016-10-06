5536d6308cf0f128bc09eba852c490927842657c
Sriram Thiagarajan <sri.sjc@gmail.com>
2016-10-06 11:40:01 +0530

    remove semicolons to comply with standard code style

--- bin/language-checker.js
+++ bin/language-checker.js
@@ -1,17 +1,17 @@
 // Utility to run files through Alex.js (http://alexjs.com/)
 // run using `npm run alex`
-var fs = require('fs');
-var path = require('path');
-var toVFile = require('to-vfile');
-var argv = require('argv');
-var alex = require('alex');
-var colors = require('colors/safe');
+var fs = require('fs')
+var path = require('path')
+var toVFile = require('to-vfile')
+var argv = require('argv')
+var alex = require('alex')
+var colors = require('colors/safe')
 
 colors.setTheme({
   info: 'green',
   warn: 'yellow',
   error: 'red'
-});
+})
 
 var options = [
     {
@@ -28,12 +28,12 @@ var options = [
         description: 'file to run through Alex.js',
         example: 'npm run alex -- -f _posts/2013-06-08-hacker-news-thread-about-hoodie.md'
     }
-];
+]
 
-var args = argv.option(options).run();
+var args = argv.option(options).run()
 
 if (args.options.dir) {
-    scanDir(args.options.dir);
+    scanDir(args.options.dir)
 }
 
 if (args.options.file) {
@@ -42,25 +42,25 @@ if (args.options.file) {
 
 function scanFile (file) {
     try {
-        var vfile = toVFile.readSync(file);
-        var messages = alex.markdown(vfile).messages;
+        var vfile = toVFile.readSync(file)
+        var messages = alex.markdown(vfile).messages
         if (messages.length) {
-            console.log(colors.warn(messages));
+            console.log(colors.warn(messages))
         } else {
-            console.log(colors.info('alex.js says ' + file + ' looks good!'));
+            console.log(colors.info('alex.js says ' + file + ' looks good!'))
         }
     } catch (error) {
-        console.error(colors.error('Error in reading file ' + file + ' - ' + error.message));
+        console.error(colors.error('Error in reading file ' + file + ' - ' + error.message))
     }
 }
 
 function scanDir (dir) {
     try {
-        var files = fs.readdirSync(dir);
+        var files = fs.readdirSync(dir)
         files.forEach(function (file) {
-            scanFile(dir + path.sep + file);
-        });
+            scanFile(dir + path.sep + file)
+        })
     } catch(error) {
-        console.log(colors.error('Error in reading directory ' + dir + ' - ' + error.message));
+        console.log(colors.error('Error in reading directory ' + dir + ' - ' + error.message))
     }
 }
